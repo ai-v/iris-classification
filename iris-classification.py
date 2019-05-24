5a2db66a54a90afdf9bd7b6c604d14dc36bc284f
Alisa Ivanenko <alisa.ivanenko@ynap.com>
2019-05-24 11:59:52 +0100

    retrieving public data, visualising and dividing dataset

---
+++ iris-classification.py
@@ -0,0 +1,53 @@
+# Load libraries
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset using pandas
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+print('loaded iris dataset')
+
+# shape -> (numrows, numcols)
+print(dataset.shape)
+
+# head
+print(dataset.head(20))
+
+# descriptions (look at summary of each attr)
+print(dataset.describe())
+
+# class distribution (number of instances that belongs to each class)
+print(dataset.groupby('class').size())
+
+# box and whisker plots (unvariate plots for each individual variable)
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+
+# Split-out validation dataset using model_selection from sklearn
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
